aca1352cbb62f21512392e145064fc0caf35f6ce
Sebastian (seprog) Poppenborg <sebastianpoppenborg@gmail.com>
2026-06-12 23:08:48 +0200

    added saturation and brightness/value settings

--- frame_processor.py
+++ frame_processor.py
@@ -10,7 +10,16 @@ from lifx_devices import Light
 @dataclass
 class FrameProcessor:
   lights: list[Light]
+
   mask: np.ndarray | None = None
+
+  saturation_exp: float = 1
+  saturation_factor: float = 1
+  saturation_range: tuple[float, float] = (0, 1)
+
+  value_exp: float = 1
+  value_factor: float = 1
+  value_range: tuple[float, float] = (0, 1)
 
   def ambilight_color(
     self,
@@ -23,7 +32,7 @@ class FrameProcessor:
           self.mask,
           (
             frame.shape[0] / self.mask.shape[0],
-            frame.shape[1] / self.mask.shape[1]
+            frame.shape[1] / self.mask.shape[1],
           )
         ),
         dtype=bool
@@ -31,5 +40,23 @@ class FrameProcessor:
     ]
     median_rgb = np.median(masked, axis=0)
     median_hsv = rgb_to_hsv(*median_rgb / 255)
+
+    clamped_scaled_median_hsv = (
+      median_hsv[0],
+      max(
+        self.saturation_range[0],
+        min(
+          self.saturation_range[1],
+          median_hsv[1] ** self.saturation_exp * self.saturation_factor,
+        )
+      ),
+      max(
+        self.value_range[0],
+        min(
+          self.value_range[1],
+          median_hsv[2] ** self.value_exp * self.value_factor,
+        )
+      ),
+    )
     
-    return median_hsv
+    return clamped_scaled_median_hsv
