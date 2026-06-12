e288dc219f07f3d8408a4a3f566a382a673cc32c
seprog <sebastianpoppenborg@gmail.com>
2026-06-12 07:53:36 +0200

    added payloads 501 and 510 to support MultiZone lights

--- lifx_payloads.py
+++ lifx_payloads.py
@@ -82,3 +82,42 @@ def payload_117(
     b'\xFF\xFF' if level else b'\x00\x00',
     round(duration * 1_000).to_bytes(4, byteorder='little')
   ))
+
+# https://lan.developer.lifx.com/docs/changing-a-device#setcolorzones---packet-501
+def payload_501(
+  hsv_color: tuple[float, float, float],
+  kelvin: int,
+  duration: float,
+  start_index: int,
+  end_index: int,
+  apply: int = 1
+):
+  return b''.join((
+    (102).to_bytes(2, byteorder='little'),
+    b'\x00' * 3,
+    start_index.to_bytes(2, byteorder='little'),
+    end_index.to_bytes(2, byteorder='little'),
+    _hsbk_struct(*hsv_color, kelvin),
+    round(duration * 1_000).to_bytes(4, byteorder='little'),
+    apply.to_bytes(1, byteorder='little')
+  ))
+
+# https://lan.developer.lifx.com/docs/changing-a-device#setextendedcolorzones---packet-510
+def payload_510(
+  start_index: int,
+  hsvk_colors: list[tuple[float, float, float, int]],
+  duration: float,
+  apply: int = 1,
+):
+  return b''.join((
+    (102).to_bytes(2, byteorder='little'),
+    b'\x00' * 3,
+    round(duration * 1_000).to_bytes(4, byteorder='little'),
+    apply.to_bytes(1, byteorder='little'),
+    start_index.to_bytes(2, byteorder='little'),
+    len(hsvk_colors).to_bytes(1, byteorder='little'),
+    b''.join(
+      _hsbk_struct(*hsvk_color)
+      for hsvk_color in hsvk_colors
+   )
+  ))
